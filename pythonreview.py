82200da13e88d76829b3eec61dbe9e4ea839689d
AdeemSh24-meet <139681300+AdeemSh24-meet@users.noreply.github.com>
2023-07-16 17:12:24 +0300

    Create pythonreview.py

---
+++ pythonreview.py
@@ -0,0 +1,19 @@
+def create_youtube_video(title,desc):
+ 	video = {"title": title,"description": desc, "likes": 0,"dislikes": 0,"comments":{}}
+ 	return video
+
+
+def like (video):
+	if "likes" in youtube_video :
+		likes += 1
+	return video
+
+def dislike (video):
+	if "dislikes" in youtube_video :
+		dislikes += 1
+	return video
+
+
+
+
+
